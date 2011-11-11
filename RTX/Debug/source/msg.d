54631bc1d216fa056ab0250c86637486606a22b2
Ziq Zhao <popoxee@hotmail.com>
2011-11-10 19:54:09 -0500

    Bruce - All headers and Data Structure with Implementation

---
+++ RTX/Debug/source/msg.d
@@ -0,0 +1,16 @@
+source/msg.d source/msg.o: ../source/msg.c \
+ /home/zik/Documents/Project/RTX/header/msg.h \
+ /home/zik/Documents/Project/RTX/header/global.h \
+ /home/zik/Documents/Project/RTX/header/buffer.h \
+ /home/zik/Documents/Project/RTX/header/msg.h \
+ /home/zik/Documents/Project/RTX/header/pcb.h
+
+/home/zik/Documents/Project/RTX/header/msg.h:
+
+/home/zik/Documents/Project/RTX/header/global.h:
+
+/home/zik/Documents/Project/RTX/header/buffer.h:
+
+/home/zik/Documents/Project/RTX/header/msg.h:
+
+/home/zik/Documents/Project/RTX/header/pcb.h:
---
+++ RTX/source/rtx.c
@@ -0,0 +1,10 @@
+/*
+ * rtx.c
+ *
+ *  Created on: 2011-11-07
+ *      Author: necross
+ */
+
+#include "k_rtx.h"
+
+//Ali, put your initialization here;
